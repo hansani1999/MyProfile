109cee04398b11fba5a91a7adde9f6d5ef069e73
hansani1999 <hansaniah@gmail.com>
2022-03-03 23:24:09 +0530

    SPA_JQuery - Completed CRUD operations for section Customer

--- myprojects/SPA_JQuery/controller/CustomerController.js
+++ myprojects/SPA_JQuery/controller/CustomerController.js
@@ -64,6 +64,14 @@ function deleteCustomer(cusId) {
     }
 }
 
+function searchCustomer(cusId) {
+    for (var customer of customerDB){
+        if (customer.getCustomerId()==cusId){
+            return customer;
+        }
+    }
+}
+
 function loadAllCustomers() {
     for (var customer of customerDB){
         let row = `<tr><td>${customer.getCustomerId()}</td><td>${customer.getCustomerName()}</td><td>${customer.getAddress()}</td><td>${customer.getSalary()}</td></tr>`;
